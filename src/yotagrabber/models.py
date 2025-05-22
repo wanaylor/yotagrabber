8f7c684bf9656a781b0601c9886d62e9d160b873
Andrew Naylor <wnaylor3@gatech.edu>
2025-05-21 22:08:02 -0400

    fix other scripts, add tundra hybrid

--- src/yotagrabber/models.py
+++ src/yotagrabber/models.py
@@ -1,14 +1,18 @@
 """Get a list of Toyota models from the Toyota website."""
 import json
+import datetime
 
 import pandas as pd
+import random
 import requests
+from time import sleep
 
-from yotagrabber import config, wafbypass
+import config, wafbypass
 
 # Set to True to use local data and skip requests to the Toyota website.
 USE_LOCAL_DATA_ONLY = False
 
+DEBUG_ENABLED = False
 
 def get_models_query():
     """Read models query from a file."""
@@ -33,21 +37,49 @@ def read_local_data():
 def query_toyota():
     """Query Toyota for a list of models."""
     query = get_models_query()
-
+    
     # Get headers by bypassing the WAF.
+    print("Bypassing WAF")
     headers = wafbypass.WAFBypass().run()
-
-    # Make request.
-    json_post = {"query": query}
-    url = "https://api.search-inventory.toyota.com/graphql"
-    resp = requests.post(
-        url,
-        json=json_post,
-        headers=headers,
-        timeout=15,
-    )
-
-    return resp.json()["data"]["models"]
+    print("Getting list of models")
+    tryCount = 4
+    result = None
+    # TODO: still getting many query failures even with this retry method (goes through all retires withuot success)
+    # and not sure why?  Printed resp.text does not seem to contain any readable ascii text.
+    while tryCount:
+        # Make request.
+        json_post = {"query": query}
+        url = "https://api.search-inventory.toyota.com/graphql"
+        resp = requests.post(
+            url,
+            json=json_post,
+            headers=headers,
+            timeout=60,
+        )
+        if DEBUG_ENABLED:
+            if resp is None:
+                print("query_toyota: models query resp is None")
+            else:
+                print("query_toyota: models query request headers: ", repr(resp.request.headers))
+                print("query_toyota: models query request.body: " + str(resp.request.body))
+                print("query_toyota: models query resp", repr (resp.headers), repr(resp))
+        try:
+            result = resp.json()["data"]["models"]
+            break
+        except (requests.exceptions.JSONDecodeError) as inst:
+            print ("query_toyota: Exception occurred with accessing json models list response:", str(type(inst)) + " "  + str(inst))
+            print("resp.status_code", resp.status_code)
+            print("resp.headers", resp.headers)
+            #print("resp.text", resp.text)
+            #print("resp.content", resp.content)
+            #return None
+        tryCount -= 1
+        print("query_toyota: Trying models list query again" ,  ", tryCount = " + str(tryCount))
+        tm = 7 + (6 * random.random())
+        print("sleeping", tm, " secs")
+        sleep(tm)
+        
+    return result
 
 
 def update_models():
@@ -72,7 +104,18 @@ def update_models():
         .sort_values("title", ascending=True)
         .reset_index(drop=True)
     )
-
+    # Add in any old models we can still get which are not in the current models list
+    #if datetime.date.today().year <= 2025
+    new_model_row = pd.DataFrame({'modelCode': ['rav4prime'], 'title': ['RAV4 Prime']})
+    models = pd.concat([models, new_model_row], ignore_index=True)
+    models.drop_duplicates(subset=["modelCode"], inplace=True)
+    new_model_row1 = pd.DataFrame({'modelCode': ['venza'], 'title': ['Venza']})
+    models = pd.concat([models, new_model_row1], ignore_index=True)
+    models.drop_duplicates(subset=["modelCode"], inplace=True)
+    new_model_row2 = pd.DataFrame({'modelCode': ['4runnerhybrid'], 'title': ['4Runner Hybrid']})
+    models = pd.concat([models, new_model_row2], ignore_index=True)
+    models.drop_duplicates(subset=["modelCode"], inplace=True)
+    
     # Toyota uses different names for some models when you query the graphQL API.
     # https://github.com/major/yotagrabber/issues/32
     models.loc[models["modelCode"] == "gr86", "modelCode"] = "86"
--- src/yotagrabber/wafbypass.py
+++ src/yotagrabber/wafbypass.py
@@ -1,9 +1,24 @@
 # Bypass the AWS WAF in front of the GraphQL endpoint.
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+from inputimeout import inputimeout, TimeoutOccurred
 
+
+def getUserInput(promptStr, sleepTime):
+    # Outputs the prompt if not null, and waits for a user input (with an ending CR which is not returned with the result) for the sleepTime
+    # returns a tuple (timedOut, userInput) where timedOut is True if timed out before input, otherwise userInput has the user entry
+    # without the CR
+    userInput = ""
+    timedOut = False
+    try:
+        userInput = inputimeout(prompt=promptStr, timeout=(sleepTime))
+    except TimeoutOccurred:
+        timedOut = True
+    return (timedOut, userInput)
 
 class WAFBypass:
     """Bypass the AWS WAF in front of the GraphQL endpoint."""
+    def __init__(self):
+        self.valid_headers = None
 
     def intercept_request(self, request):
         """Find the GraphQL request and save the headers."""
@@ -15,18 +30,35 @@ class WAFBypass:
 
     def get_headers(self) -> None:
         """Run a browser to get valid headers for a WAF bypass."""
-        with sync_playwright() as playwright:
-            browser = playwright.firefox.launch(headless=True)
-            context = browser.new_context(viewport={"width": 1920, "height": 1080})
-            page = context.new_page()
-            page.on("request", self.intercept_request)
-            page.goto("https://www.toyota.com/search-inventory/")
-            page.get_by_placeholder("ZIP Code").click()
-            page.get_by_placeholder("ZIP Code").fill("90210")
-            page.get_by_placeholder("ZIP Code").press("Enter")
-            page.wait_for_load_state("networkidle")
-            browser.close()
-
+        while True:
+            try:
+                self.valid_headers = None
+                with sync_playwright() as playwright:
+                    browser = playwright.firefox.launch(headless=True)
+                    try:
+                        context = browser.new_context(viewport={"width": 1920, "height": 1080})
+                        page = context.new_page()
+                        page.on("request", self.intercept_request)
+                        # pick a model that usually doesn't have much inventory to reduce response time and web page load time
+                        page.goto("https://www.toyota.com/search-inventory/model/" + "corollahatchback" + "/?zipcode=90210")
+                        #print("https://www.toyota.com/search-inventory/model/" + "corollahatchback" + "/?zipcode=90210")
+                        page.wait_for_load_state("networkidle", timeout=60000)
+                    except Exception as inst:
+                        print("Error: WAFBypass.get_headers: exception in code going to inventory page: ", str(inst))
+                    finally:
+                        browser.close()
+                if self.valid_headers is not None:
+                    break
+                else:
+                    print("Error: WAFBypass.get_headers was None")
+                    sleepTime = 60* 10
+                    print("Waiting time ", sleepTime, "secs before retrying WAF Bypass")
+                    getUserInput("Enter Cr to terminate wait early", sleepTime)
+            except Exception as inst:
+                print("Error: WAFBypass.get_headers exception", str(inst))
+                sleepTime = 60* 10
+                print("Waiting time ", sleepTime, "secs before retrying WAF Bypass")
+                getUserInput("Enter Cr to terminate wait early", sleepTime)
     def run(self):
         """Return the valid headers to bypass the WAF."""
         self.get_headers()
